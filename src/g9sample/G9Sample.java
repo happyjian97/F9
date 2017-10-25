bb9fa9e14bd78e086b4072ca6b9eaf7be494721a
B1 <B1@B133>
2017-10-25 14:12:15 +0800

    commit 1

--- src/g9sample/G9Sample.java
+++ src/g9sample/G9Sample.java
@@ -17,7 +17,7 @@ public class G9Sample {
     public static void main(String[] args) {
         System.out.println("Hello - Main");
         System.out.println("Hi - Second");
-        System.out.println("only branch");
+        
     }
     
 }
